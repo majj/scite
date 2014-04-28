35256af5c285e874c83badde38026914c8ca0e15
majj <aidear@163.com>
2014-04-28 22:14:45 +0800

    fix issues

--- scripts/mabo.lua
+++ scripts/mabo.lua
@@ -5,6 +5,8 @@
 ]]
 
 function OnSwitchFile(path)
+    local t = os.date("%Y-%m-%d %H:%M:%S", os.time()) 
+    print(t);
     print(path);
 end;
 
@@ -12,7 +14,8 @@ end;
 function get_ptag(tag)
     
     local ptag    
-    j = 0    
+    j = 0
+    -- until ?
     for i in string.gmatch(tag, "%S+") do
       j = j + 1
       ptag = i
@@ -23,16 +26,26 @@ function get_ptag(tag)
     return ptag    
 end
 
+--[[
+command.name.5.*=Menu5
+command.5.*=menu5
+command.subsystem.5.*=3
+command.mode.5.*=savebefore:no
+command.shortcut.5.*=Ctrl+5
+]]
+
 function menu5()
 
     print(scite.filename)
 
-    scite.StripShow("!'Explanation:'{}(&Search)\n'Name:'[Name](OK)(Cancel)");
+    -- 2014-04-26 21:33:27
+    -- scite.StripShow("!'Explanation:'{}(&Search)\n'Name:'[Name](OK)(Cancel)");
     
     local t = os.time();
     print(t);
 
-    print(os.date("%Y-%m-%d %H:%M:%S", t))  
+    local dt = os.date("%Y-%m-%d %H:%M:%S", t)  
+    editor:insert(editor.CurrentPos, "modified on " .. dt);
     print("menu5")
 
 end;
@@ -52,9 +65,10 @@ function OnChar(char)
     else 
         local val = dict[char]
         --pair check
-        local nextv = editor.CharAt[editor.CurrentPos]
+        -- local nextv = editor.CharAt[editor.CurrentPos]
         -- 32==space, 13 == \n
-        if val and (nextv == 32 or nextv == 13) then
+        -- and (nextv == 32 or nextv == 13) 
+        if val then
                 editor:insert(editor.CurrentPos, val);
         end;
     end;
@@ -93,6 +107,12 @@ function AddTag()
         
         -- add </tag><//tag> 
         senv = get_ptag(senv)
+        
+        -- bypass html comment modified on 2014-04-28 22:07:31
+        if(senv == '!--') then
+            return;
+        end;
+            
         if(senv) and off==0 then --senv isn't empty and off is off
             editor:insert(editor.CurrentPos,"</"..senv..'>');
         end;
